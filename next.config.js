9d66c001d98625dc8dda48d38684377c562af4d6
waleedbinaamer <waleedbin.aamer22@gmail.com>
2021-08-15 21:20:53 +0500

    first commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   reactStrictMode: true,
+  images:{
+    domains:[
+      "links.papareact.com",
+      "platfrom-lookaside.fbsdx.com",
+      "firebasestorage.googleapis.com",
+    ],
+  }
 }
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,15 @@
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    Providers.Facebook({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACE_CLIENT_SECRET
+    }),
+    // ...add more providers here
+  ],
+
+  
+})
